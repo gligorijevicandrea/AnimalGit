2c8ef3de3bfa7ae5337d24a5720a8983c3a6ba4c
Andrea Gligorijevic <gligorijevicandrea@gmail.com>
2023-04-11 22:45:38 -0500

    version 1

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        Dog dog=new Dog();
+        dog.breed="Lab";
+        dog.color="Yellow";
+        
+    }
+}
